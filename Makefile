f60942d2aea559edb8db101b63f87b28b4fd7fe6
Mokee04 <mokj0412@naver.com>
2025-09-18 17:44:31 +0900

    Add initial prompting module files
    Added params_v0_0.py with SetParams class and placeholder methods, and prompting_agents.ipynb notebook with initial structure. Also added a symlink to an external Makefile for build configuration.

---
+++ Makefile
@@ -0,0 +1,1 @@
+/Users/hwamokj/MokCamiBaseSource/Makefile
---
+++ prompting/params_v0_0.py
@@ -0,0 +1,29 @@
+class SetParams:
+    def __init__(self):
+        self.params = {
+            'counseling_guider': self.counseling_guider(),
+            'counselor': self.counselor(),
+        }
+        
+    def supervisor(self):
+        return {
+            
+        }
+        
+    def counselor(self):
+        return {
+            
+        }
+        
+    def tester(self):
+        return {
+            
+        }
+    
+    def judge(self):
+        return {
+            
+        }
+    
+    
+    
